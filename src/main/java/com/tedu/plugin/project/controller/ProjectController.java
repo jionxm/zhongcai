b53d3e323c746feab2103503f373c3c4d17da8b9
陈昊 <1416266401@qq.com>
2018-11-13 13:08:38 +0800

    fix #excel导出数据写活

--- src/main/java/com/tedu/plugin/project/controller/ProjectController.java
+++ src/main/java/com/tedu/plugin/project/controller/ProjectController.java
@@ -28,13 +28,18 @@ import org.apache.poi.hssf.usermodel.HSSFRow;
 import org.apache.poi.hssf.usermodel.HSSFSheet;
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 import org.apache.poi.hssf.util.CellRangeAddress;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.tedu.base.common.page.QueryPage;
 import com.tedu.base.engine.dao.FormMapper;
+import com.tedu.base.engine.model.FormModel;
 import com.tedu.base.engine.service.FormService;
 import com.tedu.base.file.util.io.file;
 import com.tedu.base.task.SpringUtils;
@@ -58,10 +63,28 @@ public class ProjectController  {
 
 	@RequestMapping("/export")
 	@ResponseBody
-	public void export(HttpServletRequest request,HttpServletResponse response) throws IOException{
+	public void export(HttpServletRequest request,HttpServletResponse response, FormModel formModel) throws IOException{
 		System.out.println(request.getParameter("id"));
 		
-		String fileName = "统计.xls";
+		Map<String,Object> map =  formModel.getData();
+		QueryPage qp = new QueryPage();
+		qp.setParamsByMap(map);
+		qp.getData().put("projId", request.getParameter("id"));
+		qp.setQueryParam("export/QryProjName");//查询项目名字
+		List<Map<String,Object>> projName = formService.queryBySqlId(qp);
+		System.out.println("项目名称----:"+projName);		
+		String fileName = projName.get(0).get("name")+".xls";
+		System.out.println("导出名称----:"+fileName);		
+		
+		qp.setQueryParam("count/QryQuestionById");
+		List<Map<String,Object>> question = formService.queryBySqlId(qp);//查询该项目下所有的题目
+		log.info("项目下所有题目----:"+question);
+		
+		qp.setQueryParam("count/QryQuestionResult");
+		List<Map<String,Object>> result = formService.queryBySqlId(qp);//查询该项目下所有的题目
+		log.info("答题结果----:"+result);
+		
+		
 		//创建HSSFWorkbook对象
 		HSSFWorkbook wkb = new HSSFWorkbook();
 		//创建HSSFSheet对象
@@ -70,23 +93,33 @@ public class ProjectController  {
 		HSSFRow row = sheet.createRow(0);
 		//创建HSSFCell对象
 		HSSFCell cell=row.createCell(0);
-		cell.setCellValue("测评表统计");
+		cell.setCellValue(fileName);
 		sheet.addMergedRegion(new CellRangeAddress(0,0,0,3));
 		//设置单元格的值
-		HSSFRow row2 = sheet.createRow(1);
-		row2.createCell(0).setCellValue("姓名");
-		row2.createCell(1).setCellValue("班级");    
-		row2.createCell(2).setCellValue("笔试成绩");
-		row2.createCell(3).setCellValue("机试成绩");    
-	      //在sheet里创建第三行
-	    HSSFRow row3=sheet.createRow(2);
-	    row3.createCell(0).setCellValue("李明");
-	    row3.createCell(1).setCellValue("As178");
-	    row3.createCell(2).setCellValue(87);    
-	    row3.createCell(3).setCellValue(78);
+		HSSFRow row1 = sheet.createRow(1);
+		row1.createCell(0).setCellValue("单位");
+		for(int i=0;i<question.size();i++){
+			String questionName = (String) question.get(i).get("question");
+			row1.createCell(i+1).setCellValue(questionName);			
+		}
+		
+		HSSFRow[] rows = new HSSFRow[result.size()];
+	    for(int j=0;j<result.size();j++){
+	    	rows[j] = sheet.createRow(j+2);
+	    	String resultName = null;
+	    	String testerName = (String) result.get(j).get("name");
+	    	for(int k=0;k<question.size();k++){
+	    		String quesName = question.get(k).get("question").toString();
+	    		resultName = (String) result.get(j).get(quesName);
+	    		log.info("答题结果----:"+testerName+quesName+resultName);
+	    		rows[j].createCell(0).setCellValue(testerName);
+	    		rows[j].createCell(k+1).setCellValue(resultName);
+	    	}
+	    }
+	  
 		
 	    
-		FileOutputStream output=new FileOutputStream("d:\\workbook.xls");
+		FileOutputStream output=new FileOutputStream("d:\\"+fileName+".xls");
 		wkb.write(output);
 		output.flush();
 		
